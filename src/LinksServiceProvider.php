31607f13565b1265ec22163076c43792fa24aea8
Vit Nasinec <vitnasinec@VnM1pro.local>
2025-01-22 11:57:02 +0100

    Morph map Area

--- src/LinksServiceProvider.php
+++ src/LinksServiceProvider.php
@@ -3,9 +3,12 @@
 namespace Primecorecz\Links;
 
 use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Facades\Blade;
+use Primecorecz\Links\Models\Area;
 use Primecorecz\Links\Models\Link;
 use Primecorecz\Links\Models\Position;
 use Primecorecz\Links\Models\Post;
+use Primecorecz\Links\View\Components\Links;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -17,7 +20,10 @@ class LinksServiceProvider extends PackageServiceProvider
             'magazine_link' => Link::class,
             'magazine_post' => Post::class,
             'iris_position' => Position::class,
+            'iris_area' => Area::class,
         ]);
+
+        Blade::component('primecore-links', Links::class);
     }
 
     public function configurePackage(Package $package): void
