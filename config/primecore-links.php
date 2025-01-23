91fbb28e40ef969369db3594b0e884acc2044ddc
Vit Nasinec <vitnasinec@VnM1pro.local>
2025-01-23 15:10:33 +0100

    Update config

--- config/primecore-links.php
+++ config/primecore-links.php
@@ -3,10 +3,10 @@
 // config for Primecorecz/Links
 return [
     'db' => [
-        'links_table' => env('PRIMECORE_LINKS_TABLE', 'magazine.links'),
-        'tags_table' => env('PRIMECORE_TAGS_TABLE', 'magazine.tags'),
-        'taggables_table' => env('PRIMECORE_TAGGABLES_TABLE', 'magazine.taggables'),
-        'posts_table' => env('PRIMECORE_POSTS_TABLE', 'magazine.posts'),
+        'links_table' => env('PRIMECORE_LINKS_TABLE', 'magazin.links'),
+        'tags_table' => env('PRIMECORE_TAGS_TABLE', 'magazin.tags'),
+        'taggables_table' => env('PRIMECORE_TAGGABLES_TABLE', 'magazin.taggables'),
+        'posts_table' => env('PRIMECORE_POSTS_TABLE', 'magazin.posts'),
         'positions_table' => env('PRIMECORE_POSITIONS_TABLE', 'iris.positions'),
         'areas_table' => env('PRIMECORE_AREAS_TABLE', 'iris.areas'),
     ],
